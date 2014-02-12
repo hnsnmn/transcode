f665f296a32077c1754f4df3d6ee7c85da0674a8
{hongseongmin} <{hnsnmn@gmail.com}>
2014-02-12 17:23:01 +0900

    # TranscodingService interface생성

---
+++ src/main/java/hnsnmn/TranscodingService.java
@@ -0,0 +1,12 @@
+package hnsnmn;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: hongseongmin
+ * Date: 2014. 2. 12.
+ * Time: 오후 5:20
+ * To change this template use File | Settings | File Templates.
+ */
+public interface TranscodingService {
+	public void transcode(Long jobId);
+}
