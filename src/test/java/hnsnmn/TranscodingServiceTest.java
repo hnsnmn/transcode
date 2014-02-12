44f92d7bf3bfa18af325bb543e6cf960e5d82335
{hongseongmin} <{hnsnmn@gmail.com}>
2014-02-12 13:15:19 +0900

    # mock di하는 방법은 총 3가지가 있다. 1.  @RunWith(MockitoJUnitRunner.class)를 선언하고 field객체에 @Mock을 걸어준다. 2. @Before setUp에 직접 mock(XXXX.class)라고 할당해준다. 3. @Before setUp에 MockitoAnnotations.initMocks(this);를 선언해준다.

--- src/test/java/hnsnmn/TranscodingServiceTest.java
+++ src/test/java/hnsnmn/TranscodingServiceTest.java
@@ -1,6 +1,15 @@
+package hnsnmn;
+
+import hnsnmn.service.MediaSourceCopier;
+import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 
 import java.io.File;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 /**
  * Created with IntelliJ IDEA.
@@ -10,11 +19,23 @@ import java.io.File;
  * To change this template use File | Settings | File Templates.
  */
 public class TranscodingServiceTest {
+	@Mock
+	private MediaSourceCopier mediaSourceCopier;
+
+	@Before
+	public void setUp() {
+//		mediaSourceCopier = mock(MediaSourceCopier.class);
+		MockitoAnnotations.initMocks(this);
+	}
+
 
 	@Test
 	public void transcodeSuccessfully() {
 		// 미디어 원본으로부터 파일을 로컬에 복사한다.
 		Long jobId = new Long(1);
+		File mockMultimediaFile = mock(File.class);
+		when(mediaSourceCopier.copy(jobId)).thenReturn(mockMultimediaFile);
+
 		File multimediaFile = copyMultimediaSourceToLocal(jobId);
 		// 로컬에 복사된 파일을 변환처리한다.
 		// 로컬에 복사된 파일로부터 이미지를 추출한다.
@@ -23,7 +44,7 @@ public class TranscodingServiceTest {
 	}
 
 	private File copyMultimediaSourceToLocal(Long jobId) {
-		return null;
+		return mediaSourceCopier.copy(jobId);
 	}
 
 }
