75db7862254f6f173a7def7ac1e8033103ec37b5
{hongseongmin} <{hnsnmn@gmail.com}>
2014-02-17 22:05:16 +0900

    # DestinationStorage 추가

---
+++ src/main/java/hnsnmn/DestinationStorage.java
@@ -0,0 +1,15 @@
+package hnsnmn;
+
+import java.io.File;
+import java.util.List;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: hongseongmin
+ * Date: 2014. 2. 17.
+ * Time: 오후 9:53
+ * To change this template use File | Settings | File Templates.
+ */
+public interface DestinationStorage {
+	void save(List<File> multimediaFiles, List<File> thumbnails);
+}
