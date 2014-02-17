8b23fc8ff422d4f00487502b7d9f7abb9329e1d8
{hongseongmin} <{hnsnmn@gmail.com}>
2014-02-17 22:05:40 +0900

    # MediaSourceFile 인터페이스 추가

---
+++ src/main/java/hnsnmn/MediaSourceFile.java
@@ -0,0 +1,14 @@
+package hnsnmn;
+
+import java.io.File;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: hongseongmin
+ * Date: 2014. 2. 17.
+ * Time: 오후 4:23
+ * To change this template use File | Settings | File Templates.
+ */
+public interface MediaSourceFile {
+	File getSourceFile();
+}
