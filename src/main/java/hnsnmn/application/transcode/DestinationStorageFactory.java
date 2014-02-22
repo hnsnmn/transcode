8cb64946d97b5197df197100c3d8affbfda8ed56
{hongseongmin} <{hnsnmn@gmail.com}>
2014-02-22 09:58:44 +0900

    # DestinationStorageFactory, MediaSourceFileFactory 추가

---
+++ src/main/java/hnsnmn/application/transcode/DestinationStorageFactory.java
@@ -0,0 +1,14 @@
+package hnsnmn.application.transcode;
+
+import hnsnmn.domain.job.DestinationStorage;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: hongseongmin
+ * Date: 2014. 2. 20.
+ * Time: 오후 2:24
+ * To change this template use File | Settings | File Templates.
+ */
+public interface DestinationStorageFactory {
+	DestinationStorage create(String storage);
+}
---
+++ src/main/java/hnsnmn/application/transcode/MediaSourceFileFactory.java
@@ -0,0 +1,14 @@
+package hnsnmn.application.transcode;
+
+import hnsnmn.domain.job.MediaSourceFile;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: hongseongmin
+ * Date: 2014. 2. 20.
+ * Time: 오후 2:12
+ * To change this template use File | Settings | File Templates.
+ */
+public interface MediaSourceFileFactory {
+	MediaSourceFile create(String mediaSource);
+}
