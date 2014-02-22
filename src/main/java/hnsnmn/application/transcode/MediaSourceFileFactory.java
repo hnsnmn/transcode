3ba510f84e7d508af4b00604992fcddcad11c34f
{hongseongmin} <{hnsnmn@gmail.com}>
2014-02-22 16:37:36 +0900

    # MediaSourceFileFactory DEFAULT를 통해 MediaSourceFileFactory 객체 생성

--- src/main/java/hnsnmn/application/transcode/MediaSourceFileFactory.java
+++ src/main/java/hnsnmn/application/transcode/MediaSourceFileFactory.java
@@ -1,5 +1,6 @@
 package hnsnmn.application.transcode;
 
+import hnsnmn.domain.job.LocalStorageMediaSourceFile;
 import hnsnmn.domain.job.MediaSourceFile;
 
 /**
@@ -11,4 +12,15 @@ import hnsnmn.domain.job.MediaSourceFile;
  */
 public interface MediaSourceFileFactory {
 	MediaSourceFile create(String mediaSource);
+
+	MediaSourceFileFactory DEFAULT = new MediaSourceFileFactory() {
+		@Override
+		public MediaSourceFile create(String mediaSource) {
+			if (mediaSource.startsWith("file://")) {
+				String filePath = mediaSource.substring("file://".length());
+				return new LocalStorageMediaSourceFile(filePath);
+			}
+			throw new IllegalArgumentException("not supported media source : " + mediaSource);
+		}
+	};
 }
