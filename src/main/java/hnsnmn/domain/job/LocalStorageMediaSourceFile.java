085fad2903065e3d5d9fd1a72b43d0e83eb51237
{hongseongmin} <{hnsnmn@gmail.com}>
2014-02-22 16:36:20 +0900

    # MediaSourceFile의 구현체 LocalStorageMediaSourceFile 작성

---
+++ src/main/java/hnsnmn/domain/job/LocalStorageMediaSourceFile.java
@@ -0,0 +1,23 @@
+package hnsnmn.domain.job;
+
+import java.io.File;
+
+/**
+* Created with IntelliJ IDEA.
+* User: hongseongmin
+* Date: 2014. 2. 22.
+* Time: 오후 4:26
+* To change this template use File | Settings | File Templates.
+*/
+public class LocalStorageMediaSourceFile implements MediaSourceFile {
+	private String filePath;
+
+	public LocalStorageMediaSourceFile(String filePath) {
+		this.filePath = filePath;
+	}
+
+	@Override
+	public File getSourceFile() {
+		return new File(filePath);
+	}
+}
