6aaed5bdbe2cf5f6fd43aadebe0183a8b4dabdbd
{hongseongmin} <{hnsnmn@gmail.com}>
2014-02-24 13:27:38 +0900

    # DefaultMediaSourceFileFactory추가

---
+++ src/main/java/hnsnmn/domain/job/DefaultMediaSourceFileFactory.java
@@ -0,0 +1,19 @@
+package hnsnmn.domain.job;
+
+/**
+* Created with IntelliJ IDEA.
+* User: hongseongmin
+* Date: 2014. 2. 24.
+* Time: 오전 10:26
+* To change this template use File | Settings | File Templates.
+*/
+public class DefaultMediaSourceFileFactory implements MediaSourceFileFactory {
+	@Override
+	public MediaSourceFile create(String mediaSource) {
+		if (mediaSource.startsWith("file://")) {
+			String filePath = mediaSource.substring("file://".length());
+			return new LocalStorageMediaSourceFile(filePath);
+		}
+		throw new IllegalArgumentException("not supported media source : " + mediaSource);
+	}
+}
