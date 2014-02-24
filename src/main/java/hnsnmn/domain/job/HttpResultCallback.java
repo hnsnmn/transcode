f67fcb3c9aa22bc566b50499b56d4f26035a800f
{hongseongmin} <{hnsnmn@gmail.com}>
2014-02-24 22:48:50 +0900

    # HttpResultCallback 추가

---
+++ src/main/java/hnsnmn/domain/job/HttpResultCallback.java
@@ -0,0 +1,25 @@
+package hnsnmn.domain.job;
+
+/**
+* Created with IntelliJ IDEA.
+* User: hongseongmin
+* Date: 2014. 2. 24.
+* Time: 오후 10:42
+* To change this template use File | Settings | File Templates.
+*/
+public class HttpResultCallback extends ResultCallback {
+
+	HttpResultCallback(String url) {
+		super(url);
+	}
+
+	@Override
+	public void notifySuccessResult(Long id) {
+		//To change body of implemented methods use File | Settings | File Templates.
+	}
+
+	@Override
+	public void notifyFailedResult(Long jobId, Job.State lastState, String errorCause) {
+		//To change body of implemented methods use File | Settings | File Templates.
+	}
+}
