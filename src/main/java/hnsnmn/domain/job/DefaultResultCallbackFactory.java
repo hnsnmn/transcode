41bb752b9cf0123cdfafc97e54a292a843a622e0
{hongseongmin} <{hnsnmn@gmail.com}>
2014-02-24 22:49:23 +0900

    # DefaultResultCallbackFactory 구현체 생성

---
+++ src/main/java/hnsnmn/domain/job/DefaultResultCallbackFactory.java
@@ -0,0 +1,20 @@
+package hnsnmn.domain.job;
+
+import hnsnmn.application.transcode.ResultCallbackFactory;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: hongseongmin
+ * Date: 2014. 2. 24.
+ * Time: 오후 10:23
+ * To change this template use File | Settings | File Templates.
+ */
+public class DefaultResultCallbackFactory implements ResultCallbackFactory{
+	@Override
+	public ResultCallback create(String url) {
+		if (url.startsWith("http://") || url.startsWith("https://")) {
+			return new HttpResultCallback(url);
+		}
+		throw new IllegalArgumentException();
+	}
+}
