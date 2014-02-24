ee6f83def8a30b00c009dad0f90fb26e471db8d3
{hongseongmin} <{hnsnmn@gmail.com}>
2014-02-24 10:20:52 +0900

    # ResultCallback 전달 객체에 추가 - innerClass 객체 위치 변경

--- src/test/java/hnsnmn/application/transcode/AddJobServiceImplTest.java
+++ src/test/java/hnsnmn/application/transcode/AddJobServiceImplTest.java
@@ -7,12 +7,9 @@ import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 
-import java.util.List;
-
 import static junit.framework.Assert.assertNotNull;
 import static org.mockito.Matchers.any;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 /**
  * Created with IntelliJ IDEA.
@@ -29,12 +26,15 @@ public class AddJobServiceImplTest {
 	@Mock private DestinationStorage mockDestinationStorage;
 	@Mock private DestinationStorageFactory destinationStorageFactory;
 	@Mock private JobRepository jobRepository;
+	@Mock private ResultCallback mockResultCallback;
+	@Mock private ResultCallbackFactory resultCallbackFactory;
 
 	@Test
 	public void addJob() {
 		AddJobRequest request = new AddJobRequest();
 		when(mediaSourceFileFactory.create(request.getMediaSource())).thenReturn(mockMediaSourceFile);
 		when(destinationStorageFactory.create(request.getDestinationStorage())).thenReturn(mockDestinationStorage);
+		when(resultCallbackFactory.create(request.getResultCallback())).thenReturn(mockResultCallback);
 
 		final Long mockJobId = new Long(1);
 		Job mockSavedJob = mock(Job.class);
@@ -43,70 +43,13 @@ public class AddJobServiceImplTest {
 
 		AddJobService addJobService = new AddJobServiceImpl(
 				mediaSourceFileFactory, destinationStorageFactory,
-				jobRepository);
+				resultCallbackFactory, jobRepository);
 		Long jobId = addJobService.addJob(request);
 		assertNotNull(jobId);
+		verify(jobRepository, only()).save(any(Job.class));
+		verify(mediaSourceFileFactory, only()).create(request.getMediaSource());
+		verify(destinationStorageFactory, only()).create(request.getDestinationStorage());
+		verify(resultCallbackFactory, only()).create(request.getResultCallback());
 	}
 
-	private class AddJobRequest {
-		private String mediaSourceFile;
-		private String destinationStorage;
-		private List<OutputFormat> outputFormats;
-
-		public String getMediaSource() {
-			return mediaSourceFile;
-		}
-
-		public String getDestinationStorage() {
-			return destinationStorage;
-		}
-
-		public List<OutputFormat> getOutputFormats() {
-			return outputFormats;
-		}
-
-		private void setMediaSourceFile(String mediaSourceFile) {
-			this.mediaSourceFile = mediaSourceFile;
-		}
-
-		private void setDestinationStorage(String destinationStorage) {
-			this.destinationStorage = destinationStorage;
-		}
-		public void setOutputFormats(List<OutputFormat> outputFormats) {
-			this.outputFormats = outputFormats;
-		}
-	}
-
-	private class AddJobServiceImpl implements AddJobService {
-		private final MediaSourceFileFactory mediaSourceFileFactory;
-		private final DestinationStorageFactory destinationStorageFactory;
-		private final JobRepository jobRepository;
-
-		@Override
-		public Long addJob(AddJobRequest request) {
-			Job job = createJob(request);
-			Job savedJob = saveJob(job);
-			return savedJob.getId();
-		}
-
-		private Job saveJob(Job job) {
-			return jobRepository.save(job);
-		}
-
-		private Job createJob(AddJobRequest request) {
-			MediaSourceFile mediaSourceFile = mediaSourceFileFactory.create(request.getMediaSource());
-			DestinationStorage destinationStorage = destinationStorageFactory.create(request.getDestinationStorage());
-			return new Job(mediaSourceFile, destinationStorage, request.getOutputFormats());
-		}
-
-		public AddJobServiceImpl(MediaSourceFileFactory mediaSourceFileFactory, DestinationStorageFactory destinationStorageFactory, JobRepository jobRepository) {
-			this.mediaSourceFileFactory = mediaSourceFileFactory;
-			this.destinationStorageFactory = destinationStorageFactory;
-			this.jobRepository = jobRepository;
-		}
-	}
-
-	private interface AddJobService {
-		Long addJob(AddJobRequest request);
-	}
 }
