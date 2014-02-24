86611c4d24c5e27e54dde91bd259ece0b17bb9f8
{hongseongmin} <{hnsnmn@gmail.com}>
2014-02-24 13:31:31 +0900

    # DEFAULT를 통해 생성한 객체는 삭제하고 같은 역할을 DefaultMediaSourceFileFactory에서 실행

--- src/main/java/hnsnmn/domain/job/MediaSourceFileFactory.java
+++ src/main/java/hnsnmn/domain/job/MediaSourceFileFactory.java
@@ -9,15 +9,4 @@ package hnsnmn.domain.job;
  */
 public interface MediaSourceFileFactory {
 	MediaSourceFile create(String mediaSource);
-
-	MediaSourceFileFactory DEFAULT = new MediaSourceFileFactory() {
-		@Override
-		public MediaSourceFile create(String mediaSource) {
-			if (mediaSource.startsWith("file://")) {
-				String filePath = mediaSource.substring("file://".length());
-				return new LocalStorageMediaSourceFile(filePath);
-			}
-			throw new IllegalArgumentException("not supported media source : " + mediaSource);
-		}
-	};
 }
