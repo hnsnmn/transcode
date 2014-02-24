d710e2bbb782c2f098449744242053e7a8665bf4
{hongseongmin} <{hnsnmn@gmail.com}>
2014-02-24 22:49:47 +0900

    # 추상 클래스로 전환

--- src/main/java/hnsnmn/domain/job/ResultCallback.java
+++ src/main/java/hnsnmn/domain/job/ResultCallback.java
@@ -1,15 +1,26 @@
 package hnsnmn.domain.job;
 
-import static hnsnmn.domain.job.Job.State;
+import static hnsnmn.domain.job.Job.*;
 
 /**
  * Created with IntelliJ IDEA.
  * User: hongseongmin
- * Date: 2014. 2. 20.
- * Time: 오전 10:10
+ * Date: 2014. 2. 24.
+ * Time: 오후 10:34
  * To change this template use File | Settings | File Templates.
  */
-public interface ResultCallback {
-	void notifySuccessResult(Long id);
-	void notifyFailedResult(long jobId, State state, String exception);
+public abstract class ResultCallback {
+	private String url;
+
+	public ResultCallback(String url) {
+		this.url = url;
+	}
+
+	public String getUrl() {
+		return url;
+	}
+
+	public abstract void notifySuccessResult(Long jobId);
+
+	public abstract void notifyFailedResult(Long jobId, State lastState, String errorCause);
 }
