3220639f2ad27a560a83d026d298211094cb760c
{hongseongmin} <{hnsnmn@gmail.com}>
2014-02-24 10:19:48 +0900

    # ResultCallbackFactory interface 구현

---
+++ src/main/java/hnsnmn/application/transcode/ResultCallbackFactory.java
@@ -0,0 +1,14 @@
+package hnsnmn.application.transcode;
+
+import hnsnmn.domain.job.ResultCallback;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: hongseongmin
+ * Date: 2014. 2. 24.
+ * Time: 오전 9:39
+ * To change this template use File | Settings | File Templates.
+ */
+public interface ResultCallbackFactory {
+	ResultCallback create(String resultCallback);
+}
