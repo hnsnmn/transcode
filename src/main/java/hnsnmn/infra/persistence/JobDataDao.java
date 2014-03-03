d1eaf961de9261a8a07876a81a0c9a06c4af7fc4
{hongseongmin} <{hnsnmn@gmail.com}>
2014-03-03 16:25:11 +0900

    # exceptionOccured db연동 추가

--- src/main/java/hnsnmn/infra/persistence/JobDataDao.java
+++ src/main/java/hnsnmn/infra/persistence/JobDataDao.java
@@ -1,6 +1,10 @@
 package hnsnmn.infra.persistence;
 
+import hnsnmn.domain.job.Job;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  * Created with IntelliJ IDEA.
@@ -13,4 +17,14 @@ public interface JobDataDao extends Repository<JobData, Long> {
 	public JobData save(JobData jobData);
 
 	public JobData findById(Long id);
+
+	@Transactional
+	@Modifying
+	@Query("update JobData j set j.state = ?2 where j.id = ?1")
+	public int updateState(Long id, Job.State newState);
+
+	@Transactional
+	@Modifying
+	@Query("update JobData j set j.exceptionMessage = ?2 where j.id = ?1")
+	public void updateExceptionMessage(Long id, String exceptionMessage);
 }
