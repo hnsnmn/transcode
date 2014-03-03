e6bc39b54f1463637c80da5139167a8cabd97241
{hongseongmin} <{hnsnmn@gmail.com}>
2014-03-03 15:50:10 +0900

    # JobImpl 구현

---
+++ src/main/java/hnsnmn/infra/persistence/JobImpl.java
@@ -0,0 +1,32 @@
+package hnsnmn.infra.persistence;
+
+import hnsnmn.domain.job.*;
+
+import java.util.List;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: hongseongmin
+ * Date: 2014. 3. 3.
+ * Time: 오후 2:04
+ * To change this template use File | Settings | File Templates.
+ */
+public class JobImpl extends Job {
+	private JobDataDao jobDataDao;
+
+	public JobImpl(JobDataDao jobDataDao, Long id, State state,
+				   MediaSourceFile mediaSourceFile,
+				   DestinationStorage destinationStorage,
+				   List<OutputFormat> outputFormats,
+				   ResultCallback callback,
+				   String errorMessage) {
+		super(id, state, mediaSourceFile, destinationStorage, outputFormats,
+				callback, errorMessage);
+		this.jobDataDao = jobDataDao;
+	}
+
+	@Override
+	protected void changeState(State newState) {
+		super.changeState(newState);
+	}
+}
